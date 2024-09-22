ef69457441120224becc5ba9c4010c4837989b4b
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2024-09-22 08:48:22 +0100

    updatingAngularLandingComponent

--- ccgc-frontend/src/app/landing-page/landing-page.component.ts
+++ ccgc-frontend/src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   templateUrl: './landing-page.component.html',
+  imports: [
+    MatTab,
+    MatTabGroup
+  ],
   styleUrls: ['./landing-page.component.css'] // Fixed 'styleUrl' to 'styleUrls'
 })
 export class LandingPageComponent {
