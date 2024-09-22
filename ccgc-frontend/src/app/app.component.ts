4165dbf3871f01a27733b3f852045c0d9ecfa63a
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2024-09-22 14:07:05 +0100

    addingFooterComponent

--- ccgc-frontend/src/app/app.component.ts
+++ ccgc-frontend/src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import {HeaderComponent} from "./shared/header/header.component";
+import {FooterComponent} from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, HeaderComponent], // Import RouterOutlet and your components directly
+  imports: [RouterOutlet, LandingPageComponent, HeaderComponent, FooterComponent], // Import RouterOutlet and your components directly
   template: `
     <app-header></app-header>
     <app-landing-page></app-landing-page>
+    <app-footer></app-footer>
     <router-outlet></router-outlet> <!-- Router outlet to render routed components -->
   `,
   styleUrls: ['./app.component.css']
