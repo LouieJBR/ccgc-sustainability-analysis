5931647dd0380d21769a0d8e4f36e5d5737bee63
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2024-09-23 17:38:53 +0100

    Auth0WorkingUserLoginLinksFromLanding

--- ccgc-frontend/src/app/shared/header/header.component.ts
+++ ccgc-frontend/src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {NgIf} from "@angular/common";
+import {Component, Inject} from '@angular/core';
+import {DOCUMENT, NgIf} from "@angular/common";
+import {AuthService} from "@auth0/auth0-angular";
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,26 @@ import {NgIf} from "@angular/common";
 })
 export class HeaderComponent {
   isLoggedIn = false;
-}
+  userName: string | null = null;
+
+
+  //The isLoggedIn variable tracks whether the user is authenticated by subscribing to auth.isAuthenticated$
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {
+    this.auth.isAuthenticated$.subscribe(isAuthenticated => {
+      this.isLoggedIn = isAuthenticated;
+
+      if (isAuthenticated) {
+        this.auth.user$.subscribe(user => {
+            this.userName = user?.name || null;
+            console.log(user)
+        });
+      } else {
+        this.userName = null; // Reset when logged out
+      }
+    });
+  }
+
+  //fires onclick of login button
+  login() {
+    this.auth.loginWithRedirect();
+  }}
